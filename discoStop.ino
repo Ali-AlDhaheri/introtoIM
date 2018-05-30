ad511b1189f4bd76a3ee09310858488162a9287d
Ali-AlDhaheri <31647294+Ali-AlDhaheri@users.noreply.github.com>
2018-05-30 05:28:11 +0400

    party time

---
+++ discoStop.ino
@@ -0,0 +1,105 @@
+/*
+SparkFun Inventor’s Kit
+Circuit 5A - Motor Basics
+
+Learn how to control one motor with the motor driver. 
+
+This sketch was written by SparkFun Electronics, with lots of help from the Arduino community.
+This code is completely free for any use.
+
+View circuit diagram and instructions at: https://learn.sparkfun.com/tutorials/sparkfun-inventors-kit-experiment-guide---v40
+Download drawings and code at: https://github.com/sparkfun/SIK-Guide-Code
+*/
+
+//PIN VARIABLES
+//the motor will be controlled by the motor A pins on the motor driver
+const int AIN1 = 13;           //control pin 1 on the motor driver for the right motor
+const int AIN2 = 12;            //control pin 2 on the motor driver for the right motor
+const int PWMA = 11;            //speed control pin on the motor driver for the right motor
+
+const int trigPin = 6;
+const int echoPin = 5;
+
+float distance = 0;            //variable to store the distance measured by the distance sensor
+
+int switchPin = 7;             //switch to turn the robot on and off
+
+//VARIABLES
+int motorSpeed = 150;       //starting speed for the motor
+
+void setup() {
+  pinMode(trigPin, OUTPUT);       //this pin will send ultrasonic pulses out from the distance sensor
+  pinMode(echoPin, INPUT);        //this pin will sense when the pulses reflect back to the distance sensor
+
+  pinMode(switchPin, INPUT_PULLUP);   //set this as a pullup to sense whether the switch is flipped
+
+  //set the motor contro pins as outputs
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(PWMA, OUTPUT);
+
+  Serial.begin(9600);                       //begin serial communication with the computer
+
+  Serial.println("Enter motor speed (0-255)... ");    //Prompt to get input in the serial monitor.
+}
+
+void loop() {
+  distance = getDistance();
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" in");              // print the units
+  
+  if (Serial.available() > 0){          //if the user has entered something in the serial monitor
+    motorSpeed = Serial.parseInt();     //set the motor speed equal to the number in the serial message
+
+    Serial.print("Motor Speed: ");      //print the speed that the motor is set to run at
+    Serial.println(motorSpeed);
+  }
+
+  if(digitalRead(7) == LOW && distance >= 70){            //if the switch is on...
+    spinMotor(motorSpeed);
+  } else{                               //if the switch is off...
+      spinMotor(0);                   //turn the motor off
+  }
+
+
+}
+
+/********************************************************************************/
+void spinMotor(int motorSpeed)                       //function for driving the right motor
+{
+  if (motorSpeed > 0)                                 //if the motor should drive forward (positive speed)
+  {
+    digitalWrite(AIN1, HIGH);                         //set pin 1 to high
+    digitalWrite(AIN2, LOW);                          //set pin 2 to low
+  }
+  else if (motorSpeed < 0)                            //if the motor should drive backwar (negative speed)
+  {
+    digitalWrite(AIN1, LOW);                          //set pin 1 to low
+    digitalWrite(AIN2, HIGH);                         //set pin 2 to high
+  }
+  else                                                //if the motor should stop
+  {
+    digitalWrite(AIN1, LOW);                          //set pin 1 to low
+    digitalWrite(AIN2, LOW);                          //set pin 2 to low
+  }
+  analogWrite(PWMA, abs(motorSpeed));                 //now that the motor direction is set, drive it at the entered speed
+}
+float getDistance()
+{
+  float echoTime;                   //variable to store the time it takes for a ping to bounce off an object
+  float calcualtedDistance;         //variable to store the distance calculated from the echo time
+
+  //send out an ultrasonic pulse that's 10ms long
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10); 
+  digitalWrite(trigPin, LOW);
+
+  echoTime = pulseIn(echoPin, HIGH);      //use the pulsein command to see how long it takes for the
+                                          //pulse to bounce back to the sensor
+
+  calcualtedDistance = echoTime / 148.0;  //calculate the distance of the object that reflected the pulse (half the bounce time multiplied by the speed of sound)
+
+  return calcualtedDistance;              //send back the distance that was calculated
+}
+
